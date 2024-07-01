43143a84416f1599e0783d2a61501ad603f9afed
unknown <suraz.kuru@gmail.com>
2024-07-01 15:52:19 +0545

    start

--- 03MODEL/models/E-commerce/product.models.js
+++ 03MODEL/models/E-commerce/product.models.js
@@ -1,29 +1,37 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     description: {
-        required: true,
-        types: String,
+      required: true,
+      types: String,
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     productImage: {
-        type: String
+      type: String,
     },
     price: {
-        type: Number,
-        default: 0
-    }, 
+      type: Number,
+      default: 0,
+    },
     stock: {
-        default: 0,
-        type: Number
+      default: 0,
+      type: Number,
     },
     category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category"
-    }
-},{timestamps: true})
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Product = mongoose.model("Product", productSchema)
+export const Product = mongoose.model("Product", productSchema);
